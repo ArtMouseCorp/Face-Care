7e05c02bfbfa5e2340444006dd622d8be218f36a
Kolya Libak <libaknikolajj@gmail.com>
2021-09-04 15:42:28 +0200

    Added tab bar icons

---
+++ Face Care/Model/Article.swift
@@ -0,0 +1,29 @@
+import Foundation
+
+struct Article {
+    
+    let title: String
+    let image: String
+    let text: String
+    
+    static let `default` = Article(title: "Title", image: "image", text: "Text")
+    
+    static let articles = [
+        Article(title: "Лифтинг сыворотки для лица", image: "atricle-1", text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book"),
+        Article(title: "Освежающие маски для лица", image: "atricle-2", text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book"),
+        Article(title: "Энзимная пудра для умывания", image: "atricle-3", text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book"),
+        Article(title: "Что такое мезотерапия для лица?", image: "atricle-4", text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book")
+    ]
+    
+}
+
+/*
+ //           _._
+ //        .-'   `
+ //      __|__
+ //     /     \
+ //     |()_()|
+ //     \{o o}/
+ //      =\o/=
+ //       ^ ^
+ */
---
+++ Face Care/Screens/Main/Controllers/TabBarController.swift
@@ -0,0 +1,47 @@
+import UIKit
+
+class TabBarController: UITabBarController {
+
+    // MARK: - Awake functions
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureUI()
+        localize()
+    }
+    
+    // MARK: - Custom functions
+    
+    private func configureUI() {
+        
+        tabBar.layer.shadowColor = UIColor.FCBlack.withAlphaComponent(0.08).cgColor
+        tabBar.layer.shadowOpacity = 0.5
+        tabBar.layer.shadowOffset = CGSize(width: 0, height: -1)
+        tabBar.layer.shadowRadius = 3
+        self.tabBar.layer.borderColor = UIColor.clear.cgColor
+        self.tabBar.layer.borderWidth = 0
+        self.tabBar.clipsToBounds = false
+        self.tabBar.backgroundColor = UIColor.white
+        UITabBar.appearance().shadowImage = UIImage()
+        UITabBar.appearance().backgroundImage = UIImage()
+        
+    }
+    
+    private func localize() {
+        tabBar.items?[0].title = "Main"
+        tabBar.items?[1].title = "Progress"
+        tabBar.items?[2].title = "Articles"
+    }
+
+}
+
+/*
+ //           _._
+ //        .-'   `
+ //      __|__
+ //     /     \
+ //     |()_()|
+ //     \{o o}/
+ //      =\o/=
+ //       ^ ^
+ */
